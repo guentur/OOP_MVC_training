847971fb2eae7d34e6bf23189a43c9cc9185f1e6
Kirill_Peretiatko <Kyr.nk.go@gmail.com>
2020-07-13 22:07:03 +0300

    29-34

---
+++ education_oop/exercises/(29)static_metods and property/(30)static_ property/Class.php
@@ -0,0 +1,14 @@
+<?php
+
+class Num
+{
+    public static $num1;
+    public static $num2;
+
+    function __construct($num1, $num2) {
+        self::$num1 = $num1;
+        self::$num2 = $num2;
+    }
+}
+
+?>
---
+++ education_oop/exercises/(29)static_metods and property/(30)static_ property/index.php
@@ -0,0 +1,7 @@
+<?php
+require_once 'Class.php';
+$num = new Num(1, 3);
+
+echo Num::$num1 + Num::$num2;
+
+?>
